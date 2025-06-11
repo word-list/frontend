de83b8abbb1158a093002569ef4db1e23415459c
Jonathan Gaul <jon@gaul.tech>
2025-06-11 20:38:56 +0000

    Invalidate cache on deploy; add scripting

---
+++ www/script.js
@@ -0,0 +1,36 @@
+document.querySelector("#generateButton").addEventListener("click", async function (event) {
+    event.preventDefault();
+
+    const formData = new FormData(document.querySelector("form"));
+    const params = new URLSearchParams(formData).toString();
+
+    try {
+        const response = await fetch(`https://staging.wordlist.gaul.tech/api/wordlist?${params}`);
+        const data = await response.json();
+
+        updateTable(data.words);
+    }
+    catch (error) {
+        console.error("Error fetching word list:", error);
+    }
+})
+
+function updateTable(words) {
+    const tbody = document.querySelector(".word-list tbody");
+    tbody.innerHTML = "";
+
+    if (words.length === 0) {
+        return;
+    }
+
+    words.forEach(word => {
+        const row = document.createElement("tr");
+        row.innerHTML = `
+            <td>${word.text}</td>
+            <td>${word.commonness}</td>
+            <td>${word.offensiveness}</td>
+            <td>${word.sentiment}</td>
+        `;
+        tbody.appendChild(row);
+    });
+}
